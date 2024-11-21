fd5909c7f717998164ecca4db33a3a2e125ee569
Ritesh Persaud <riteshpersaud2003@gmail.com>
2024-11-21 13:36:01 -0500

    Simplified Java Fibonacci

--- Fibonacci_Sequence/Fibonacci.java
+++ Fibonacci_Sequence/Fibonacci.java
@@ -10,27 +10,21 @@ public class Fibonacci {
         }
 
         List<Integer> sequence = new ArrayList<>();
-        int n1 = 0, n2 = 1;
+        int n1 = 0;
+        int n2 = 1;
 
         for (int i = 0; i < iterations; i++) {
-            if (i == 0) {
-                sequence.add(n1);
-            } else if (i == 1) {
-                sequence.add(n2);
-            } else {
-                int nextTerm = n1 + n2;
-                sequence.add(nextTerm);
-                n1 = n2;
-                n2 = nextTerm;
-            }
+            sequence.add(n1);
+            int nextTerm = n1 + n2;
+            n1 = n2;
+            n2 = nextTerm;
         }
         return sequence;
     }
 
     public static void main(String[] args) {
         try {
-            List<Integer> result = fibonacci(10);
-            System.out.println(result);
+            System.out.println(fibonacci(5));
         } catch (IllegalArgumentException e) {
             System.err.println(e.getMessage());
         }
